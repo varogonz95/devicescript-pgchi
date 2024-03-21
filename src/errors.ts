edc9f346454f7be00d51bbd1ae8667ede70f326e
Álvaro González <varogonz95@gmail.com>
2024-03-21 09:33:49 -0600

    register device api (#16)

--- src/errors.ts
+++
@@ -1,5 +0,0 @@
-export class UnsupportedSensorServerError extends Error {
-    constructor(sensorType: string) {
-        super(`Unsupported server for sensor type ${sensorType}`);
-    }
-}
--- src/routine-orchestrator.ts
+++ src/routine-orchestrator.ts
@@ -1,71 +1,75 @@
-import { Actions, SetValue } from "./actions";
-import { RoutinesConfig } from "./config";
-import { PeripheralRecords } from "./peripherals";
-import { ConditionType, RoutineCondition, isAllOfCondition, isAnyOfCondition, isRangeConditionType, isScalarConditionType } from "./routines";
+import { Actions, SetValue } from "./models/actions";
+import { RoutinesConfig } from "./models/config";
+import { PeripheralRecords } from "./models/peripherals";
+import { ConditionType, RoutineCondition, isAllOfCondition, isAnyOfCondition, isRangeConditionType, isScalarConditionType } from "./models/routines";
 
-export async function runAll(peripherals: PeripheralRecords, routines: RoutinesConfig) {
-    for (const key in routines) {
-        const { conditions, actions } = routines[key]
-        const reading = await peripherals[key].read()
-        const meetsConditions = conditionsMet(conditions, reading)
+export class RoutineOrchestrator {
 
-        if (isSetValueAction(actions)) {
-            const { target, value, otherwise, durationOptions } = actions.setValue
-            const peripheral = peripherals[target]
-            const newValue = meetsConditions ? value : otherwise
-            await peripheral.write(newValue, durationOptions)
+    public static async runAll(peripherals: PeripheralRecords, routines: RoutinesConfig) {
+        for (const key in routines) {
+            const { conditions, actions } = routines[key]
+            const reading = await peripherals[key].read()
+            const meetsConditions = this.conditionsMet(conditions, reading)
+
+            if (this.isSetValueAction(actions)) {
+                const { target, value, otherwise, durationOptions } = actions.setValue
+                const peripheral = peripherals[target]
+                const newValue = meetsConditions ? value : otherwise
+                await peripheral.write(newValue, durationOptions)
+            }
+
+            // if (actions.sendEmail) {
+            //     // TODO: Implement email notification
+            // }
         }
-
-        // if (actions.sendEmail) {
-        //     // TODO: Implement email notification
-        // }
-    }
-}
-
-export function conditionsMet<T>(conditions: RoutineCondition, value: T) {
-    if (isAllOfCondition(conditions)) {
-        return allOfConditionsMet(conditions.allOf, value);
     }
 
-    if (isAnyOfCondition(conditions)) {
-        return anyOfConditionsMet(conditions.anyOf, value);
+    private static conditionsMet<T>(conditions: RoutineCondition, value: T) {
+        if (isAllOfCondition(conditions)) {
+            return this.allOfConditionsMet(conditions.allOf, value);
+        }
+
+        if (isAnyOfCondition(conditions)) {
+            return this.anyOfConditionsMet(conditions.anyOf, value);
+        }
+
+        return false;
     }
 
-    return false;
+    private static evaluateConditions(conditions: ConditionType[], value: any) {
+        return conditions.map(condition => {
+            if (isScalarConditionType(condition)) {
+                return (condition.equals && value === condition.equals)
+                    || (condition.greaterOrEqualsTo && value >= condition.greaterOrEqualsTo)
+                    || (condition.greaterThan && value > condition.greaterThan)
+                    || (condition.lessOrEqualsTo && value <= condition.lessOrEqualsTo)
+                    || (condition.lessThan && value < condition.lessThan)
+                    || (condition.notEquals && value !== condition.notEquals)
+
+            }
+
+            if (isRangeConditionType(condition)) {
+                const [lower, upper] = condition.between
+                return lower <= value && value <= upper
+            }
+
+            return false
+        })
+    }
+
+    private static allOfConditionsMet(conditions: ConditionType[], value: any) {
+        return this
+            .evaluateConditions(conditions, value)
+            .every(assertion => assertion === true)
+    }
+
+    private static anyOfConditionsMet(conditions: ConditionType[], value: any) {
+        return this
+            .evaluateConditions(conditions, value)
+            .some(assertion => assertion === true)
+    }
+
+    private static isSetValueAction(actions: Actions): actions is SetValue {
+        return (actions as SetValue).setValue !== undefined
+    }
 }
-
-export function evaluateConditions(conditions: ConditionType[], value: any) {
-    return conditions.map(condition => {
-        if (isScalarConditionType(condition)) {
-            return (condition.equals && value === condition.equals)
-                || (condition.greaterOrEqualsTo && value >= condition.greaterOrEqualsTo)
-                || (condition.greaterThan && value > condition.greaterThan)
-                || (condition.lessOrEqualsTo && value <= condition.lessOrEqualsTo)
-                || (condition.lessThan && value < condition.lessThan)
-                || (condition.notEquals && value !== condition.notEquals)
-
-        }
-
-        if (isRangeConditionType(condition)) {
-            const [lower, upper] = condition.between
-            return lower <= value && value <= upper
-        }
-
-        return false
-    })
-}
-
-export function allOfConditionsMet(conditions: ConditionType[], value: any) {
-    return evaluateConditions(conditions, value)
-        .every(assertion => assertion === true)
-}
-
-export function anyOfConditionsMet(conditions: ConditionType[], value: any) {
-    return evaluateConditions(conditions, value)
-        .some(assertion => assertion === true)
-}
-
-function isSetValueAction(actions: Actions): actions is SetValue {
-    return (actions as SetValue).setValue !== undefined
-}
-
